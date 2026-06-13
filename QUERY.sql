3ef87a928dfd160bfc7b703a5722d65d7dd878c8
Mohammad Ali <thedev.mohammadali@gmail.com>
2026-06-13 22:53:04 +0600

    Add Query 1

--- QUERY.sql
+++ QUERY.sql
@@ -177,3 +177,17 @@ VALUES
     (503, 2, 101, 'A-13', 'Confirmed', 150.00),
     (504, 2, 101, NULL, NULL, 150.00),
     (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+-- =========================================================================
+-- QUERIES
+-- =========================================================================
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM
+    Matches
+WHERE
+    tournament_category = 'Champions League'
+    AND match_status = 'Available';
