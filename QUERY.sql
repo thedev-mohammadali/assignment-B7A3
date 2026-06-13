651004cd1a0c33daf9ec3b09d8239e6e1e4744b6
Mohammad Ali <thedev.mohammadali@gmail.com>
2026-06-13 23:09:18 +0600

    Add Query 3

--- QUERY.sql
+++ QUERY.sql
@@ -202,3 +202,14 @@ FROM
 WHERE
     full_name ILIKE 'Tanvir%'
     OR full_name ILIKE '%Haque%';
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS "systematic_status"
+FROM
+    Bookings
+WHERE
+    payment_status IS NULL;
